907f8847591e9fbbf081b56d717fe37bd3854422
MorFix <morc@arescft.com>
2021-08-03 21:58:30 +0300

    fix bugs

--- src/app/partnertv/src/main/java/com/morfix/partnertv/ui/PlaybackVideoFragment.kt
+++ src/app/partnertv/src/main/java/com/morfix/partnertv/ui/PlaybackVideoFragment.kt
@@ -2,72 +2,111 @@ package com.morfix.partnertv.ui
 
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.leanback.app.VideoSupportFragment
 import androidx.leanback.app.VideoSupportFragmentGlueHost
 import androidx.leanback.media.PlaybackTransportControlGlue
 import androidx.leanback.widget.PlaybackControlsRow
-import com.google.android.exoplayer2.C
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.ext.leanback.LeanbackPlayerAdapter
 import com.google.android.exoplayer2.util.MimeTypes
 import com.morfix.partnertv.lib.TvPartner
 import com.morfix.partnertv.lib.data.UserData
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import java.lang.Exception
 
 /** Handles video playback with media controls. */
+@ExperimentalCoroutinesApi
 class PlaybackVideoFragment : VideoSupportFragment() {
 
     private lateinit var mTransportControlGlue: PlaybackTransportControlGlue<LeanbackPlayerAdapter>
+    private var exoPlayer: ExoPlayer? = null
 
-    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
-
-    }
-
-    @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         val (id, title, description, _, _) =
             activity?.intent?.getSerializableExtra("Movie") as PartnerMedia
 
-        val glueHost = VideoSupportFragmentGlueHost(this@PlaybackVideoFragment)
-        val exoPlayer = SimpleExoPlayer.Builder(requireContext()).build()
+        exoPlayer = createExoPlayer(description)
 
-        val playerAdapter = LeanbackPlayerAdapter(requireContext(), exoPlayer, 100)
+        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
+        val playerAdapter = LeanbackPlayerAdapter(requireContext(), exoPlayer!!, 100)
         playerAdapter.setRepeatAction(PlaybackControlsRow.RepeatAction.INDEX_NONE)
 
         mTransportControlGlue = PlaybackTransportControlGlue(activity, playerAdapter)
-        mTransportControlGlue.host = glueHost
+        mTransportControlGlue.host = VideoSupportFragmentGlueHost(this)
         mTransportControlGlue.title = title
-        mTransportControlGlue.subtitle = description
+        mTransportControlGlue.subtitle = "Loading..."
         mTransportControlGlue.playWhenPrepared()
 
-        runBlocking {
-            launch {
-                val session = TvPartner().createSession(id, UserData())
-                if (session != null) {
-                    val mediaItem = MediaItem.Builder()
-                        .setUri(session.dashUrl)
-                        .setDrmLicenseUri(session.drm)
-                        .setDrmUuid(C.WIDEVINE_UUID)
-                        .setMimeType(MimeTypes.APPLICATION_MPD)
-                        .setDrmMultiSession(true)
-                        .build()
-
-                    exoPlayer.addMediaItem(mediaItem)
-                    exoPlayer.prepare()
-                }
-            }
-        }
+        initChannel(id)
     }
 
     override fun onPause() {
         super.onPause()
         mTransportControlGlue.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        exoPlayer?.release()
+    }
+
+    private fun createExoPlayer(subtitle: String?): ExoPlayer {
+        val player = SimpleExoPlayer.Builder(requireContext()).build()
+        player.prepare()
+
+        val listener : Player.Listener = object : Player.Listener {
+            override fun onPlayerError(error: ExoPlaybackException) {
+                super.onPlayerError(error)
+
+                close(error)
+            }
+
+            override fun onRenderedFirstFrame() {
+                super.onRenderedFirstFrame()
+
+                mTransportControlGlue.subtitle = subtitle
+            }
+        }
+
+        player.addListener(listener)
+
+        return player
+    }
+
+    private fun initChannel(id: Long) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val mainScope = CoroutineScope(Dispatchers.Main)
+
+            val session = TvPartner().createSession(id, UserData())
+            if (session != null) {
+                val mediaItem = MediaItem.Builder()
+                    .setUri(session.dashUrl)
+                    .setDrmLicenseUri(session.drm)
+                    .setDrmUuid(C.WIDEVINE_UUID)
+                    .setMimeType(MimeTypes.APPLICATION_MPD)
+                    .setDrmMultiSession(true)
+                    .build()
+
+                mainScope.launch {
+                    exoPlayer?.addMediaItem(mediaItem)
+                }
+            } else {
+                close(Exception("Can't find a link for this channel"))
+            }
+        }
+    }
+
+    private fun close(e: Throwable) {
+        CoroutineScope(Dispatchers.Main).launch {
+            Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG)
+                .show()
+
+            requireActivity().finish()
+        }
     }
 }
